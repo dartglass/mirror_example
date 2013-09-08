30c7b3063abe5023ba9225fe00c44fbed9b88502
Adam Singer <financeCoding@gmail.com>
2013-09-08 15:13:37 -0700

    clean up

--- web/mirror_example.dart
+++ web/mirror_example.dart
@@ -1,4 +1,5 @@
-import 'dart:html';
+import 'dart:html' as html;
+//import "package:html5lib/dom.dart" as dom;
 import "package:google_oauth2_client/google_oauth2_browser.dart";
 import "package:google_mirror_v1_api/mirror_v1_api_browser.dart";
 import "package:google_mirror_v1_api/mirror_v1_api_client.dart";
@@ -17,22 +18,34 @@ insertCard(Token token) {
   Mirror mirror = new Mirror(auth);
   mirror.makeAuthRequests = true;
 
-//  Map attributes = {
-//    "html": "<article class=\"photo cover-only\">\n  <img src=\"http://www.glassfrogger.com/images/game-field.jpg\" height=\"100%\" width=\"100%\">\n  <div class=\"photo-overlay\"></div>\n  <section>\n    <p class=\"text-auto-size\"><strong class=\"white\">Google Glass</strong> <em class=\"blue\">Dartified!</em></p>\n  </section>\n<footer>\n    <img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAVCAMAAACeyVWkAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAJxQTFRFAAAAAIvMdsvDAIvMdsvDAIvMdsvDLaTJAIvMOqnHdsvDAIvMdsvDAIvMKaLJdsvDAIvMAIvMdsvDAIvMdsvDdsvDAIvMAIvMAZnFdsvDAILHAIPHAITIAIXJAIfKAIjKAIrLAIrMAIvMAJXHAJjFC5i/I6HENr2yOb6zPr+0TsK4UsO5WbnEWcW8Xsa9Yse+Zsi/asjAc8rCdsvDdt4SRQAAABp0Uk5TABAQICAwMFBgYGBwcICAgI+vr7+/z9/v7+97IXGnAAAAqUlEQVQYV13QxxaCQBBE0VZkjBgAGVEBaVEUM/P//yaTGg5vV3dZANTCZ9BvFAoR93kVC9FnthW6uIPTJ7UkdHaXvS2LXKNBURInyDXPsShbzjU7XCpxhooDVGo5QcQAJmjUco64AY/UcIrowYCTaj5KBZeTaj5JBTc6l11OlQKMf497y1ahefFb3TQfcqtM/fipJF/X9gnDon6/ah/aDDfNOgosNA2b8QdGciZlh/U93AAAAABJRU5ErkJggg==\" class=\"left\">\n    <p>Dart Hacking</p>\n  </footer>\n </article>\n<article class=\"auto-paginate\">\n  <ol class=\"text-x-small\">\n  <strong>Instructions:</strong><hr>  <li>First item</li>\n    <li>Second item</li>\n    <li>Third item</li>\n    <li>Fourth item</li>\n  </ol>\n</article>\n",
-//    "menuItems": [
-//    {
-//      "action": "VIEW_WEBSITE",
-//      "payload": "http://www.google.com"
-//    }
-//    ],
-//    "notification": {
-//      "level": "DEFAULT"
-//    }
-//  };
+  String html = """
+<article class="photo cover-only">  
+  <img src="http://www.glassfrogger.com/images/game-field.jpg" height="100%" width="100%">  
+  <div class="photo-overlay"></div>  
+  <section>    
+    <p class="text-auto-size">
+      <strong class="white">Google Glass</strong> 
+      <em class="blue">Dartified!</em>
+    </p>  
+  </section>
+  <footer>    
+    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAVCAMAAACeyVWkAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAJxQTFRFAAAAAIvMdsvDAIvMdsvDAIvMdsvDLaTJAIvMOqnHdsvDAIvMdsvDAIvMKaLJdsvDAIvMAIvMdsvDAIvMdsvDdsvDAIvMAIvMAZnFdsvDAILHAIPHAITIAIXJAIfKAIjKAIrLAIrMAIvMAJXHAJjFC5i/I6HENr2yOb6zPr+0TsK4UsO5WbnEWcW8Xsa9Yse+Zsi/asjAc8rCdsvDdt4SRQAAABp0Uk5TABAQICAwMFBgYGBwcICAgI+vr7+/z9/v7+97IXGnAAAAqUlEQVQYV13QxxaCQBBE0VZkjBgAGVEBaVEUM/P//yaTGg5vV3dZANTCZ9BvFAoR93kVC9FnthW6uIPTJ7UkdHaXvS2LXKNBURInyDXPsShbzjU7XCpxhooDVGo5QcQAJmjUco64AY/UcIrowYCTaj5KBZeTaj5JBTc6l11OlQKMf497y1ahefFb3TQfcqtM/fipJF/X9gnDon6/ah/aDDfNOgosNA2b8QdGciZlh/U93AAAAABJRU5ErkJggg==" class="left">  
 
-  // TODO: replace this with stuff from html5lib or dart:html.
-  // using html5lib can be reused from the console.
-  String html = "<article class=\"photo cover-only\">\n  <img src=\"http://www.glassfrogger.com/images/game-field.jpg\" height=\"100%\" width=\"100%\">\n  <div class=\"photo-overlay\"></div>\n  <section>\n    <p class=\"text-auto-size\"><strong class=\"white\">Google Glass</strong> <em class=\"blue\">Dartified!</em></p>\n  </section>\n<footer>\n    <img src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABUAAAAVCAMAAACeyVWkAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAAJxQTFRFAAAAAIvMdsvDAIvMdsvDAIvMdsvDLaTJAIvMOqnHdsvDAIvMdsvDAIvMKaLJdsvDAIvMAIvMdsvDAIvMdsvDdsvDAIvMAIvMAZnFdsvDAILHAIPHAITIAIXJAIfKAIjKAIrLAIrMAIvMAJXHAJjFC5i/I6HENr2yOb6zPr+0TsK4UsO5WbnEWcW8Xsa9Yse+Zsi/asjAc8rCdsvDdt4SRQAAABp0Uk5TABAQICAwMFBgYGBwcICAgI+vr7+/z9/v7+97IXGnAAAAqUlEQVQYV13QxxaCQBBE0VZkjBgAGVEBaVEUM/P//yaTGg5vV3dZANTCZ9BvFAoR93kVC9FnthW6uIPTJ7UkdHaXvS2LXKNBURInyDXPsShbzjU7XCpxhooDVGo5QcQAJmjUco64AY/UcIrowYCTaj5KBZeTaj5JBTc6l11OlQKMf497y1ahefFb3TQfcqtM/fipJF/X9gnDon6/ah/aDDfNOgosNA2b8QdGciZlh/U93AAAAABJRU5ErkJggg==\" class=\"left\">\n    <p>Dart Hacking</p>\n  </footer>\n </article>\n<article class=\"auto-paginate\">\n  <ol class=\"text-x-small\">\n  <strong>Instructions:</strong><hr>  <li>First item</li>\n    <li>Second item</li>\n    <li>Third item</li>\n    <li>Fourth item</li>\n  </ol>\n</article>\n";
+    <p>Dart Hacking</p>   
+  </footer> 
+</article>
+
+<article class="auto-paginate">  
+  <ol class="text-x-small">  
+  <strong>Instructions:</strong>
+  <hr>  
+    <li>First item</li>    
+    <li>Second item</li>    
+    <li>Third item</li>    
+    <li>Fourth item</li>  
+  </ol>
+</article>
+""";
   NotificationConfig notification = new NotificationConfig.fromJson({});
   notification.level = "DEFAULT";
   MenuItem menuItem = new MenuItem.fromJson({});
@@ -52,13 +65,13 @@ insertCard(Token token) {
 void main() {
   auth = new GoogleOAuth2(CLIENT_ID, SCOPES);
 
-  query("#sign-in").onClick.listen((e) {
+  html.query("#sign-in").onClick.listen((e) {
     print("Attempting to log you in.");
     auth.login(immediate: true).then(insertCard,
         onError: (error) => print("login error: $error"));
   });
 
-  query("#sign-out").onClick.listen((e) {
+  html.query("#sign-out").onClick.listen((e) {
     auth.logout();
     print("Signing you out.");
   });
